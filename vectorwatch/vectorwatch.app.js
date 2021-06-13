40b5d0daf7e6b56746a5a6304f1e180b9c365ef5
Niklas Amann <niklas.amann95@gmail.com>
2021-06-13 14:59:25 +0200

    Fixed screen timeout bug

--- vectorwatch/vectorwatch.app.js
+++ vectorwatch/vectorwatch.app.js
@@ -1,48 +1,48 @@
 var locale = require("locale");
 var X = 120, Y = 82;
-var compareMinutes;
+var compareDate;
 
 function update() {
-  if(new Date().getMinutes() != compareMinutes) {
+  if (new Date().getMinutes() != compareDate.getMinutes()) {
     var date = new Date();
     var minutes = date.getMinutes();
     g.reset();
     g.setFontAlign(0, -1);
     g.setFont("Vector", 80);
-    if(minutes % 10 == 0){
+    if (Math.floor(minutes / 10) != Math.floor(compareDate.getMinutes() / 10)) {
       g.clearRect(X + 21, Y + 5, X + 110, Y + 65);
       g.drawString(("0" + minutes).substr(-2), X + 71, Y);
     } else {
       g.clearRect(X + 71, Y + 5, X + 110, Y + 65);
       g.drawString(("0" + minutes).substr(-1), X + 96, Y);
     }
-    if(minutes == 0) {
+    if (date.getHours() != compareDate.getHours()) {
       var hours = date.getHours();
-      if(hours % 10 == 0){
+      if (Math.floor(hours / 10) != Math.floor(compareDate.getHours() / 10)) {
         g.clearRect(X - 110, Y + 5, X - 21, Y + 65);
         g.drawString(("0" + hours).substr(-2), X - 60, Y);
       } else {
         g.clearRect(X - 60, Y + 5, X - 21, Y + 65);
         g.drawString(("0" + hours).substr(-1), X - 35, Y);
       }
-      if(hours == 0) {
+      if (date.getDate() != compareDate.getDate()) {
         g.setFont("Vector", 25);
         g.clearRect(0, Y + 88, 240, Y + 105);
         g.drawString(`${locale.dow(date, 1).toUpperCase()} ${locale.month(date).toUpperCase()} ${date.getDate()}`, X, Y + 86);
       }
     }
-    compareMinutes = minutes;
+    compareDate = date;
   }
 }
 
-function draw(){
+function draw() {
   var date = new Date();
-  compareMinutes = date.getMinutes();
+  compareDate = date;
   g.reset();
   g.setFontAlign(0, -1);
   g.setFont("Vector", 80);
   g.drawString(("0" + date.getHours()).substr(-2), X - 60, Y);
-  g.drawString(("0" + compareMinutes).substr(-2), X + 71, Y);
+  g.drawString(("0" + compareDate.getMinutes()).substr(-2), X + 71, Y);
   g.setFont("Vector", 25);
   g.drawString(`${locale.dow(date, 1).toUpperCase()} ${locale.month(date).toUpperCase()} ${date.getDate()}`, X, Y + 86);
   g.setColor(0, 0.5, 1);
@@ -56,7 +56,7 @@ Bangle.drawWidgets();
 draw();
 let secondInterval = setInterval(update, 1000);
 
-Bangle.on('lcdPower',on=>{
+Bangle.on('lcdPower', on => {
   if (secondInterval) clearInterval(secondInterval);
   secondInterval = undefined;
   if (on) {
